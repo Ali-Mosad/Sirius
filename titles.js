566113cf5bfbaebf0dc95f4f2d73f783dbafef70
Ali-Mosad <alimosad776@gmail.com>
2024-12-16 12:09:01 +0200

    dasa

--- titles.js
+++ titles.js
@@ -74,7 +74,7 @@ document.getElementById('addTitleForm').addEventListener('submit', async (e) => 
     const tool = document.getElementById('tool').value;
 
     // The Google Apps Script URL
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbzvOmCC7yzHZ3xLqzgdK4R669V_KKr-o3NjTK8mt3Gr8YNxj0vSxCjbOWY6-eNWkSYI/exec'; // Replace with your script URL
+    const scriptUrl = 'https://script.google.com/macros/library/d/1TkotTrK3svgaYjcYPecEHjz8ApHJgsMkjL3LUdELxyQp-HpnNNatywSY/1'; // Replace with your script URL
 
     // Send data to the Google Apps Script
     const response = await fetch(scriptUrl, {
