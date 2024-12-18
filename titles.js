0f3ddb1a994a8aae1d9180ec56653802aca6a298
Ali-Mosad <alimosad776@gmail.com>
2024-12-18 22:23:22 +0200

    dsa

--- titles.js
+++ titles.js
@@ -115,5 +115,26 @@ document.getElementById("searchForm").addEventListener("submit", (event) => {
     event.preventDefault();
 });
 
+// Button Click to Scroll to Specific Section
+document.getElementById("goToمحاربين").addEventListener("click", () => {
+    const targetSection = document.getElementById("محاربين");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("goToفرسان").addEventListener("click", () => {
+    const targetSection = document.getElementById("فرسان");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("goToالإدارةعليا").addEventListener("click", () => {
+    const targetSection = document.getElementById("الإدارة العليا");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("goToأعضاء").addEventListener("click", () => {
+    const targetSection = document.getElementById("أعضاء");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+});
+
 // Initial Data Display
 updateDisplay(); // Show data from Google Sheets
